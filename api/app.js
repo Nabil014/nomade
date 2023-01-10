bb4af649d560cdd54bd8449550a6ed92ab33a21a
Nabil <nallis014@gmail.com>
2023-01-10 12:25:55 -0300

    First commit

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ const http = require("http");
 require("./db.js");
 
 app.options("*", cors());
-app.name = "API";
+app.name = "API"
 
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
